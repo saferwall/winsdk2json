1a11cddf98ba48f71055ff42eb624d2bcc3c805a
Noteworthy <lord.noteworthy@protonmail.com>
2023-09-06 15:25:45 +1000

    refactor: keep only the win32sdk headers, no need for mingw (#1)
    * remove submodule
    
    * rename parse.go to parse.old.go
    
    * update parse ast
    
    * cmd/parse.go cmd/parsev2.go
    
    * update subcommands
    
    * code update
    
    * update settings

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Windows SDK API definition in JSON
+# winsdk2json
 
-sdk2json is a go package that parses the Windows SDK (prototypes, structures, unions) to JSON format.
+sdk2json is a go package that parses the Windows SDK (function prototypes, structures, unions) to a friendlier format like JSON. The purpose of this tool was initially for the API hooking module (of saferwall sandbox) to allow an implementation of a generic hook handler that does not require knowledge of specific API prototype to know how to log them.
+
+`winsdk2json` relies on a [C compiler frontend](https://gitlab.com/cznic/cc) for parsing the C headers and convert them to a higher level objects. Those objects can be serialized formats like JSON or YAML.
 
 Here is an example:
 
@@ -31,20 +33,48 @@ Here is an example:
   },
 ```
 
-The malware sandbox hooking module makes use of this to implement a generic hook handler that does not require to implement a handler for each API we need to hook.
+## Usage
 
-## Installation
+You can download the JSON files that contains all the definitions of the Win32 APIs in the releases page. If however, you intend to generate those definitions on another custom that fits your needs, please follow the instructions below:
 
-- Install the Windows SDK
+Clone the repo:
+
+```shell
+git clone https://github.com/saferwall/winsdk2json.git
+cd winsdk2json
+git submodule init
+git submodule update
+```
+
+Build & run"
+
+```shell
+go build cli.go
+.\cli.exe -h
+
+WinSdk2JSON - a tool to parse the Windows Win32 SDK into JSON format.
+For more details see the github repo at https://github.com/saferwall/winsdk2json
+
+Usage:
+  winsdk2json [flags]
+  winsdk2json [command]
+
+Available Commands:
+  completion  Generate the autocompletion script for the specified shell
+  help        Help about any command
+  parse       Walk through the Windows SDK and parse the Win32 headers
+  version     Version number
+
+Flags:
+  -h, --help   help for winsdk2json
+```
+
+This package tries to stay up to date with the latest [Windows SDK](https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/), they are copied into the `winsdk/` folder. However, feel free to use an SDK version that is not included in this repo. The `parse` command takes a `i` argument that points to the Windows SDK headers. For example: `C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.19041.0\\`.
 
 ## Lessons Learned
 
 - SAL annotations
 - Some Windows APIs does not have a ANSI alternative: i.e OpenMutexW
-
-## Challenges
-
-- Not a consistent way of defines functions prototypes or structs.
 
 ## References
 
--- cmd/parse.go
+++ cmd/parse.go
@@ -5,45 +5,28 @@
 package cmd
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"regexp"
-	"strings"
+	"runtime"
 
-	"github.com/saferwall/winsdk2json/internal/parser"
 	"github.com/saferwall/winsdk2json/internal/utils"
 	"github.com/spf13/cobra"
+	"github.com/xlab/c-for-go/translator"
+	"modernc.org/cc/v4"
 )
 
 // Used for flags.
 var (
-	sdkumPath      string
-	sdkapiPath     string
-	hookapisPath   string
-	customhookPath string
-	printretval    bool
-	printanno      bool
-	minify         bool
+	includePath string
 )
 
 func init() {
 
-	parseCmd.Flags().StringVarP(&sdkumPath, "sdk", "", "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.19041.0\\",
-		"The path to the windows sdk directory")
-	parseCmd.Flags().StringVarP(&sdkapiPath, "sdk-api", "", ".\\sdk-api",
-		"The path to the sdk-api docs directory (https://github.com/MicrosoftDocs/sdk-api)")
-	parseCmd.Flags().StringVarP(&hookapisPath, "hookapis", "", ".\\assets\\hookapis.md",
-		"The path to a a text file thats defines which APIs to trace, new line separated.")
-	parseCmd.Flags().StringVarP(&customhookPath, "customhookapis", "", ".\\assets\\custom_hook_apis.md",
-		"The path to a a text file thats defines which APIs uses custom hook handlers")
-
-	parseCmd.Flags().BoolVarP(&printretval, "printretval", "", false, "Print return value type for each API")
-	parseCmd.Flags().BoolVarP(&printanno, "printanno", "", false, "Print list of annotation values")
-	parseCmd.Flags().BoolVarP(&minify, "minify", "m", false, "Mininify json")
+	parseCmd.Flags().StringVarP(&includePath, "include", "i", "./winsdk/10.0.22000.0",
+		"Path to the Windows Kits include directory")
 }
 
 var parseCmd = &cobra.Command{
@@ -57,255 +40,91 @@ var parseCmd = &cobra.Command{
 
 func run() {
 
-	if sdkumPath == "" {
+	if _, err := os.Stat(includePath); os.IsNotExist(err) {
+		fmt.Print("The include directory does not exist ..")
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	if !utils.Exists(sdkumPath) {
-		log.Fatal("Windows sdk directory does not exist")
-	}
-	if !utils.Exists(sdkapiPath) {
-		log.Fatal("sdk-api directory does not exist")
-	}
-	if !utils.Exists(hookapisPath) {
-		log.Fatal("hookapis.md does not exists")
-	}
-	if !utils.Exists(customhookPath) {
-		log.Fatal("customhookapis.md does not exists")
-	}
-
-	// Read the list of APIs we are interested to hook.
-	wantedAPIs, err := utils.ReadLines(hookapisPath)
+	filePath := filepath.Join("assets", "header.h")
+	code, err := utils.ReadAll(filePath)
 	if err != nil {
-		log.Fatalln(err)
-	}
-	if len(wantedAPIs) == 0 {
-		log.Fatalln("hookapis.md is empty")
+		log.Fatalf("reading header.h failed: %v", err)
 	}
 
-	// Read the list of APIs we uses that uses a custom hook handler.
-	customHookHHandlerAPIs, err := utils.ReadLines(customhookPath)
+	config, err := cc.NewConfig(runtime.GOOS, runtime.GOARCH)
 	if err != nil {
-		log.Fatalln(err)
-	}
-	if len(customHookHHandlerAPIs) == 0 {
-		log.Fatalln("customhookapis.md is empty")
+		log.Fatal(err)
 	}
 
-	// Initialize built-in compiler data types.
-	parser.InitBuiltInTypes()
+	config.HostSysIncludePaths = config.HostSysIncludePaths[:0]
+	config.IncludePaths = config.IncludePaths[:0]
+	config.SysIncludePaths = config.SysIncludePaths[:0]
 
-	// Get the list of files in the Windows SDK.
-	files, err := utils.WalkAllFilesInDir(sdkumPath)
+	config.SysIncludePaths = append(config.SysIncludePaths, includePath+"/um")
+	config.SysIncludePaths = append(config.SysIncludePaths, includePath+"/shared")
+	config.SysIncludePaths = append(config.SysIncludePaths, includePath+"/../14.29.30133/include")
+	config.SysIncludePaths = append(config.SysIncludePaths, includePath+"/ucrt")
+	config.HostSysIncludePaths = config.SysIncludePaths
+	config.IncludePaths = config.SysIncludePaths
+
+	config.Predefined += "\n#define __int64 long long\n"
+	config.Predefined += "#define __iamcu__\n"
+	config.Predefined += "#define __int32 int\n"
+	config.Predefined += "#define NTDDI_WIN7 0x06010000\n"
+	config.Predefined += "#define __forceinline __attribute__((always_inline))\n"
+	config.Predefined += "#define _AMD64_\n"
+	config.Predefined += "#define _M_AMD64\n"
+	config.Predefined += "#define __unaligned\n"
+
+	var sources []cc.Source
+	sources = append(sources, cc.Source{Name: "<predefined>", Value: config.Predefined})
+	sources = append(sources, cc.Source{Name: "<builtin>", Value: cc.Builtin})
+	sources = append(sources, cc.Source{Name: "<undefines>", Value: "\n#undef __cplusplus\n"})
+	sources = append(sources, cc.Source{Value: code})
+
+	// ast, err := cc.Parse(config, sources)
+	ast, err := cc.Translate(config, sources)
 	if err != nil {
-		log.Fatalln(err)
-	}
-	files = append(files, ".\\assets\\custom-def.h")
-
-	// Defines some vars.
-	m := make(map[string]map[string]parser.API)
-	var winStructsRaw []string
-	var winStructs []parser.Struct
-	funcPtrs := make([]string, 0)
-	var interestingHeaders = []string{
-		"\\fileapi.h", "\\processthreadsapi.h", "\\winreg.h", "\\bcrypt.h",
-		"\\winbase.h", "\\urlmon.h", "\\memoryapi.h", "\\tlhelp32.h", "\\debugapi.h",
-		"\\handleapi.h", "\\heapapi.h", "\\winsvc.h", "\\wincrypt.h", "\\wow64apiset.h",
-		"\\libloaderapi.h", "\\sysinfoapi.h", "\\synchapi.h", "\\winuser.h", "\\ioapiset.h",
-		"\\winhttp.h", "\\minwinbase.h", "\\minwindef.h", "\\winnt.h", "\\shellapi.h", "\\shlwapi.h",
-		"\\ntdef.h", "\\basetsd.h", "\\wininet.h", "winsock.h", "securitybaseapi.h", "winsock2.h",
-		"\\ws2tcpip.h", "\\corecrt_wstring.h", "\\corecrt_malloc.h", "processenv.h", "custom-def.h",
+		log.Fatalf("parse cpp source file failed with:\n%v", err)
 	}
 
-	parsedAPI := 0
-	var foundAPIs []string
-	for _, file := range files {
-		foundHdr := false
-		var prototypes []string
-		file = strings.ToLower(file)
-		for _, headerName := range interestingHeaders {
-			if strings.HasSuffix(file, headerName) {
-				foundHdr = true
-				break
-			}
-		}
-
-		if !foundHdr {
-			continue
-		}
-
-		// Read Win32 include API headers.
-		log.Printf("Processing %s ...\n", file)
-		data, err := utils.ReadAll(file)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		// Parse typedefs.
-		parser.ParseTypedefs(data)
-
-		// Start parsing all struct in header file.
-		a, b := parser.GetAllStructs(data)
-		winStructsRaw = append(winStructsRaw, a...)
-		winStructs = append(winStructs, b...)
-
-		// Extract all function pointers.
-		functionPointers := parser.ParseFunctionPointers(string(data))
-		funcPtrs = append(funcPtrs, functionPointers...)
-
-		// Grab all API prototypes.
-		// 1. Ignore: FORCEINLINE
-		r := regexp.MustCompile(parser.RegAPIs)
-		matches := r.FindAllString(string(data), -1)
-		for _, v := range matches {
-			prototype := utils.RemoveAnnotations(v)
-			prototype = utils.StandardizeSpaces(prototype)
-			prototype = utils.Standardize(prototype)
-			prototypes = append(prototypes, prototype)
-
-			if strings.Contains(v, "getaddrinfo") {
-				log.Print(v)
-			}
-
-			// Only parse APIs we want to hook.
-			if strings.HasPrefix(prototype, "SHSTDAPI_(HINSTANCE)") {
-				prototype = strings.ReplaceAll(prototype, "SHSTDAPI_(HINSTANCE)", "")
-				prototype = "HINSTANCE SHSTDAPI" + prototype
-			} else if strings.HasPrefix(prototype, "LWSTDAPI_(LPCSTR)") {
-				prototype = strings.ReplaceAll(prototype, "LWSTDAPI_(LPCSTR)", "")
-				prototype = "LPCSTR LWSTDAPI" + prototype
-			} else if strings.HasPrefix(prototype, "LWSTDAPI_(BOOL)") {
-				prototype = strings.ReplaceAll(prototype, "LWSTDAPI_(BOOL)", "")
-				prototype = "BOOL LWSTDAPI" + prototype
-			} else if strings.HasPrefix(prototype, "LWSTDAPI_(PCSTR)") {
-				prototype = strings.ReplaceAll(prototype, "LWSTDAPI_(PCSTR)", "")
-				prototype = "PCSTR LWSTDAPI" + prototype
-			} else if strings.HasPrefix(prototype, "LWSTDAPI_(PCWSTR)") {
-				prototype = strings.ReplaceAll(prototype, "LWSTDAPI_(PCWSTR)", "")
-				prototype = "PCWSTR LWSTDAPI" + prototype
-			} else if strings.Contains(prototype, "// deprecated: annotation is as good as it gets") {
-				prototype = strings.ReplaceAll(prototype, "// deprecated: annotation is as good as it gets", "")
-
-			}
-			mProto := utils.RegSubMatchToMapString(parser.RegProto, prototype)
-			if !utils.StringInSlice(mProto["ApiName"], wantedAPIs) && !utils.StringInSlice(mProto["ApiName"], customHookHHandlerAPIs) {
-				continue
-			}
-
-			// Parse the API prototype.
-			papi := parser.ParseAPI(prototype)
-
-			// Find which DLL this API belongs to. Unfortunately, the sdk does
-			// not give you this information, we look into the sdk-api markdown
-			// docs instead. (Normally, we could have parsed everything from
-			// the md files, but they are missing the parameters type!).
-			dllname, err := utils.GetDLLName(file, papi.Name, sdkapiPath)
-			if err != nil {
-				log.Printf("Failed to get the DLL name for: %s", papi.Name)
-				dllname = "ntdll.dll"
-			}
-			if _, ok := m[dllname]; !ok {
-				m[dllname] = make(map[string]parser.API)
-			}
-			m[dllname][papi.Name] = papi
-			parsedAPI++
-			foundAPIs = append(foundAPIs, papi.Name)
-		}
-
-		// Write raw prototypes to a text file.
-		if len(prototypes) > 0 {
-			_, err = utils.WriteStrSliceToFile("./dump/prototypes-"+filepath.Base(file)+".inc", prototypes)
-			if err != nil {
-				log.Fatalf("Failed to dump prototype %s", filepath.Base(file)+".inc")
-			}
-		}
-	}
-
-	// Append the NTDLL definitions.
-	ntdll_json, err := utils.ReadAll("./assets/ntdll.json")
+	myTranslator, err := translator.New(&translator.Config{})
 	if err != nil {
-		log.Fatalf("Failed to read NTDLL definitions,, err: %v", err)
-	}
-	ntdll_defs := make(map[string]map[string]parser.API)
-	err = json.Unmarshal([]byte(ntdll_json), &ntdll_defs)
-	if err != nil {
-		log.Fatalf("Failed to unmarshall NTDLL definitions,, err: %v", err)
-	}
-	m["ntdll.dll"] = ntdll_defs["ntdll.dll"]
-
-	// Marshall and write to json file.
-	if len(m) > 0 {
-		data, _ := json.MarshalIndent(m, "", " ")
-		_, err = utils.WriteBytesFile("./assets/apis.json", bytes.NewReader(data))
-		if err != nil {
-			log.Fatalf("Failed to dump apis.json")
-		}
+		log.Fatalf("failed to create new translator: %v", err)
 	}
 
-	// Write struct results.
-	utils.WriteStrSliceToFile("./dump/winstructs.h", winStructsRaw)
-	d, _ := json.MarshalIndent(winStructs, "", " ")
-	utils.WriteBytesFile("./assets/structs.json", bytes.NewReader(d))
+	myTranslator.Learn(ast)
 
-	if printretval {
-		for dll, v := range m {
-			log.Printf("DLL: %s\n", dll)
-			log.Println("====================")
-			for api, vv := range v {
-				log.Printf("API: %s:%s() => %s\n", vv.CallingConvention, api, vv.ReturnValueType)
-				if !utils.StringInSlice(api, wantedAPIs) {
-					log.Printf("Not found")
-				}
-			}
-		}
-	}
+	//r := strings.NewReader(ast.TranslationUnit.String())
+	//utils.WriteBytesFile("ast.txt", r)
+	//log.Print(ast)
+	//log.Print(ast.TranslationUnit.String())
 
-	log.Printf("Parsed API count: %d, Wanted API Count: %d", parsedAPI, len(wantedAPIs))
-
-	// Init custom types.
-	parser.InitCustomTypes(winStructs)
-
-	if printanno || minify {
-		data, err := utils.ReadAll("./assets/apis.json")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		apis := make(map[string]map[string]parser.API)
-		err = json.Unmarshal(data, &apis)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		if printanno {
-			var annotations []string
-			var types []string
-			for _, v := range apis {
-				for _, vv := range v {
-					for _, param := range vv.Params {
-						if !utils.StringInSlice(param.Annotation, annotations) {
-							annotations = append(annotations, param.Annotation)
-							// log.Println(param.Annotation)
-						}
-
-						if !utils.StringInSlice(param.Type, types) {
-							types = append(types, param.Type)
-							log.Println(param.Type)
+	i := 0
+	for _, d := range myTranslator.Declares() {
+		if d.Name != "CreateFileA" {
+			funcSpec, ok := d.Spec.(*translator.CFunctionSpec)
+			if ok {
+				retSpec, ok := funcSpec.Return.(*translator.CTypeSpec)
+				if ok {
+					returnString := retSpec.Raw
+					if retSpec.Raw == "" {
+						returnString = retSpec.Base
+					}
+					fmt.Printf("\n%s %s (", returnString, d.Name)
+					i++
+					for _, param := range funcSpec.Params {
+						paramSpec, ok := param.Spec.(*translator.CTypeSpec)
+						if ok {
+							fmt.Printf("%s %s,", paramSpec.Raw, param.Name)
 						}
 					}
+					fmt.Printf(")")
 				}
 			}
-		}
 
-		if minify {
-			// Minifi APIs.
-			data, _ := json.Marshal(parser.MinifyAPIs(apis, customHookHHandlerAPIs))
-			utils.WriteBytesFile("./assets/mini-apis.json", bytes.NewReader(data))
-
-			// Minify Structs/Unions.
-			data, _ = json.Marshal(parser.MinifyStructAndUnions(winStructs))
-			utils.WriteBytesFile("./assets/mini-structs.json", bytes.NewReader(data))
 		}
-		os.Exit(0)
 	}
+	log.Printf("SUCCESS %d", i)
 }
